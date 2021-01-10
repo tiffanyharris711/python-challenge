603b0718f43a5180271f5822ac7aceaa0417d517
tiffanyharris711 <tiffanyaharris711@gmail.com>
2021-01-09 19:27:08 -0600

    finished pybank python code, may need to come back and incorporate lists

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,17 +2,50 @@ import os
 import csv
 
 #set the initial values
-count = 0
+count = 0 #count total number of rows to account for total votes
+khan_votes = 0 
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
+max_vote = 0
 
-csvpath = os.path.join('/Users','tiffanyharris','Desktop','git_things','myGitRepos','python-challenge','PyPoll','Resources', 'election_data.csv')
-
+#csvpath = os.path.join('/Users','tiffanyharris','Desktop','git_things','myGitRepos','python-challenge','PyPoll','Resources', 'election_data.csv')
+csvpath = os.path.join('.','Resources','election_data.csv')
 with open(csvpath, newline='') as csvfile:
-    csvreader = csv.DictReader(csvfile) #Use DictReader instead of reader to skip header
+    csvreader = csv.reader(csvfile)
+    next(csvreader) #skip header
+
     for row in csvreader:
         count += 1  #number of rows after the header
-
-
+        if row[2] == "Khan": #this if stmt adds the number of votes by candidate
+            khan_votes += 1
+        elif row[2] == "Correy":
+            correy_votes += 1
+        elif row[2] == "Li":
+            li_votes += 1
+        elif row[2] == "O'Tooley":
+            otooley_votes += 1
+        if khan_votes > max_vote: #this if stmt compares the different vote counts to determine the max and declare the winner
+            max_vote = khan_votes
+            winner = "Khan"
+            if correy_votes > max_vote:
+                max_vote = correy_votes
+                winner = "Correy"
+                if li_votes > max_vote:
+                    max_vote = li_votes
+                    winner = "Li"
+                    if otooley_votes > max_vote:
+                        max_vote = otooley_votes
+                        winner = "O'Tooley" 
+    
 print("Election Results")
 print("--------------------")  
 print("Total Votes: " + str(count))
+print("--------------------")  
+print("Khan Votes: " + str(round(int(khan_votes)/int(count)*100,3)) + "%  (" + str(khan_votes)+")")
+print("Correy Votes: " + str(round(int(correy_votes)/int(count)*100,3)) + "%  (" + str(correy_votes)+")")
+print("Li Votes: " + str(round(int(li_votes)/int(count)*100,3)) + "%  (" + str(li_votes)+")")
+print("O'Tooley Votes: " + str(round(int(otooley_votes)/int(count)*100,3)) + "%  (" + str(otooley_votes)+")")
+print("--------------------")  
+print("Winner: " + winner)
 print("--------------------")  
