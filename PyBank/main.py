a0cf393544fee0a4d7bcf5a19475b8d144d884ad
tiffanyharris711 <tiffanyaharris711@gmail.com>
2021-01-09 18:48:12 -0600

    completed first part of homework in pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,17 +1,43 @@
 import os
 import csv
+import statistics
 
 #set the initial values
 net_total = 0
 count = 0
+this_row =0
+next_row =0
+change_dataset = []
+greatest_increase = 0
+greatest_decrease = 0
 
-csvpath = os.path.join('/Users','tiffanyharris','Desktop','git_things','myGitRepos','python-challenge','PyBank','Resources', 'budget_data.csv')
+#csvpath = os.path.join('/Users','tiffanyharris','Desktop','git_things','myGitRepos','python-challenge','PyBank','Resources', 'budget_data.csv')
+csvpath = os.path.join('.','Resources', 'budget_data.csv')
 
 with open(csvpath, newline='') as csvfile:
-    csvreader = csv.DictReader(csvfile) #Use DictReader instead of reader to skip header
+    csvreader = csv.reader(csvfile) 
+    next(csvreader) #skip header row
     for row in csvreader:
         count += 1  #number of rows after the header
-        net_total += int(row["Profit/Losses"])  #total of the profit/losses
+        net_total += int(row[1])  #total of the profit/losses
+        if count > 1: #start calculating the change with the second row
+            next_row = row[1]
+            change = int(next_row) - int(this_row) #cast as integers since they are initially strings
+            change_dataset.append(change) #put all of the change values in a list to calc mean later
+            if change > greatest_increase:
+                greatest_increase = change
+                greatest_increase_per = row[0]
+            if change < greatest_decrease:
+                greatest_decrease = change
+                greatest_decrease_per = row[0]
+        this_row = row[1] #store the first row and then store the next row on the second loop to subtract them
+        
+average_change = round(statistics.mean(change_dataset),2) #use stats module to calc mean and round to 2 decimals
 
+print("Financial Analyis")
+print("--------------------")
 print("Total Months: " + str(count))
 print("Total: $" + str(net_total))
+print("Average Change: $" + str(average_change))
+print("Greatest Increase in Profits: " + str(greatest_increase_per) + " ($" + str(greatest_increase)+")")
+print("Greatest Decrease in Profits: " + str(greatest_decrease_per) + " ($" + str(greatest_decrease)+")")
